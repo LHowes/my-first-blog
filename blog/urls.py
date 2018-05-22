e362d8628dce44c736750e7a3c6fd3c0d0eeb9a7
LHowes <laura.howes@embl.de>
2018-05-22 17:36:13 +0200

    Added view and template for detailed blog post

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 #adding first URL pattern
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
